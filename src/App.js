7681c922a7643e2598bb284de72c1762d99eb36f
yun <ysr@geoize.net>
2022-10-14 15:22:46 +0900

    main image change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import mainImg from './static/image/main_image.jpg';
 import './App.css';
 
+const showAlert = () => {
+    alert('개발중인 페이지입니다...');
+};
+
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={mainImg} className="App-logo" alt="logo" onClick="alert(11);" />
+                <a href="return false;" onClick={showAlert}>
+                    <img src={mainImg} className="App-logo" alt="logo" />
+                </a>
                 <p>🎉 상렬 그리고 은비, 결혼합니다 🎉</p>
             </header>
         </div>
