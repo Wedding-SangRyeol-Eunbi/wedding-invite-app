c959d14d59f2e4232365b6d36348ad68b256f82f
yun <ysr@geoize.net>
2022-10-18 11:40:30 +0900

    캘린더 수정

--- src/components/WeddingDate.js
+++ src/components/WeddingDate.js
@@ -8,16 +8,15 @@ function WeddingDate() {
 
     return (
         <div>
-            <p>2023-01-15</p>
             <Calendar
                 value={selectDate}
                 calendarType="US"
                 // minDate={new Date('2023-01-01')}
                 // maxDate={new Date('2023-01-31')}
-                nextLabel={null}
-                next2Label={'오후 2시'}
                 prevLabel={null}
                 prev2Label={'2023년 1월 15일 (일)'}
+                nextLabel={null}
+                next2Label={'오후 2시'}
                 // showNavigation={false}
                 showNeighboringMonth={false}
                 locale={'ko'}
