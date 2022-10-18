4bf800eb2c2d341518905eea67bcba30d8bf50f4
yun <ysr@geoize.net>
2022-10-18 19:00:52 +0900

    상단 메뉴  추가중

---
+++ src/components/TopNavigator.js
@@ -0,0 +1,11 @@
+import '../static/css/TopNavigator.css';
+
+function TopNavigator() {
+    return (
+        <div id="topNavigatorDiv">
+            <a href="#">상렬 ♥️ 은비</a> |<a href="#">예식일</a> |<a href="#">오시는길</a>
+        </div>
+    );
+}
+
+export default TopNavigator;
