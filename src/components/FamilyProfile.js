c83bcbb26b352787213c3cadc98ec67ca03c8747
YunSangRyeol <ysr9393@naver.com>
2022-10-20 22:43:58 +0900

    CI/CD 테스트

--- src/components/FamilyProfile.js
+++ src/components/FamilyProfile.js
@@ -3,8 +3,8 @@ function FamilyProfile() {
         <div id="familyProfileDiv">
             <p className="subTitle">연락처</p>
             <div>
-                <span>신랑측</span>
-                <span>신부측</span>
+                <span>신랑측 </span>
+                <span>신부측 </span>
             </div>
         </div>
     );
