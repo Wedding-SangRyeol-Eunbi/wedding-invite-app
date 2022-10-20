e9df0c823c9ecad860e775e5f17ae9ba88cc947e
YunSangRyeol <ysr9393@naver.com>
2022-10-20 22:42:55 +0900

    .

--- src/components/FamilyProfile.js
+++ src/components/FamilyProfile.js
@@ -2,6 +2,10 @@ function FamilyProfile() {
     return (
         <div id="familyProfileDiv">
             <p className="subTitle">연락처</p>
+            <div>
+                <span>신랑측</span>
+                <span>신부측</span>
+            </div>
         </div>
     );
 }
