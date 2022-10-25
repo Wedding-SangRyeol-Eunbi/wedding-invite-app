fd3f2299f24fb2a71aad1044c570c5fb8aa63312
YunSangRyeol <ysr9393@naver.com>
2022-10-26 00:04:37 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,46 +1,73 @@
-import React from 'react';
-import mainImg from './static/img/img02_sizing.jpg';
-import Confetti from 'react-confetti';
-import KakaoShare from './components/KakaoShare';
-import WeddingDate from './components/WeddingDate';
-import './static/css/App.css';
-import TopNavigator from './components/TopNavigator';
-import KakaoMap from './components/KakaoMap';
-import FamilyProfile from './components/FamilyProfile';
-import WomanIcon from './static/img/icon/womanIcon.png';
-import ManIcon from './static/img/icon/manIcon.png';
+import React from "react";
+import mainImg from "./static/img/img05.jpg";
+import Confetti from "react-confetti";
+import KakaoShare from "./components/KakaoShare";
+import WeddingDate from "./components/WeddingDate";
+import "./static/css/App.css";
+import TopNavigator from "./components/TopNavigator";
+import KakaoMap from "./components/KakaoMap";
+import FamilyProfile from "./components/FamilyProfile";
+import WomanIcon from "./static/img/icon/womanIcon.png";
+import ManIcon from "./static/img/icon/manIcon.png";
 
 function App() {
-    const nameStyle = {
-        'font-weight': 'bold',
-        'font-size': '18px',
-    };
-    return (
-        <div className="App">
-            <Confetti
-                height={2000}
-                numberOfPieces="50"
-                friction={0.96}
-                initialVelocityX={60}
-                // colors={['#FF0000']}
-                opacity={0.4}
-            />
-            <TopNavigator />
-            <img src={mainImg} className="main-image" id="main" alt="logo" />
-            <p>
-                <img src={ManIcon} className="prefixIcon" />
-                <b>윤재순</b>, <b>최옥자</b>의 아들 <span className="name">상렬</span>
-            </p>
-            <p>
-                <img src={WomanIcon} className="prefixIcon" />
-                <b>문기현</b>, <b>한은경</b>의 딸 <span className="name">은비</span>
-            </p>
-            <WeddingDate />
-            <KakaoMap />
-            <FamilyProfile />
-            <KakaoShare />
-        </div>
-    );
+  const nameStyle = {
+    "font-weight": "bold",
+    "font-size": "18px",
+  };
+  return (
+    <div className="App">
+      <Confetti
+        height={2000}
+        numberOfPieces="50"
+        friction={0.96}
+        initialVelocityX={60}
+        // colors={['#FF0000']}
+        opacity={0.4}
+      />
+      <TopNavigator />
+      <img src={mainImg} className="main-image" id="main" alt="logo" />
+      <div className="greetingMessage">
+        <span>
+          2023년 첫 달, 우리들의 세 번째 겨울
+          <br />
+          문은비 그리고 윤상렬 결혼합니다.
+          <br />
+          <br />
+          새로운 해가 시작되는 1월
+          <br />
+          둘이 함께 나아가는 첫걸음을 <br />
+          여러분들의 축하와 함께 한발 내디디려 합니다.
+          <br />
+          저희 둘의 첫 시작을 함게 축복해 주세요.
+        </span>
+      </div>
+      <hr
+        style={{
+          width: 280,
+          height: 1,
+          borderWidth: 0,
+          backgroundColor: "gray",
+        }}
+      />
+      <div className="parentsName">
+        <p>
+          <img src={ManIcon} className="prefixIcon" />
+          <span className="name">윤재순, 최옥자</span>의 아들{" "}
+          <span className="name">상렬</span>
+        </p>
+        <p>
+          <img src={WomanIcon} className="prefixIcon" />
+          <span className="name">문기현, 한은경</span>의 딸{" "}
+          <span className="name">은비</span>
+        </p>
+      </div>
+      <WeddingDate />
+      <KakaoMap />
+      <FamilyProfile />
+      <KakaoShare />
+    </div>
+  );
 }
 
 export default App;
--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,156 +1,224 @@
-import React, {useState, useEffect} from 'react';
-import {Map, MapMarker} from 'react-kakao-maps-sdk';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
-import ToastMessage from '../components/common/ToastMessage.js';
-import coupleIcon from '../static/img/icon/coupleCharacter.png';
-import copyIcon from '../static/img/icon/copyIcon.png';
-import kakaonavi from '../static/img/icon/kakaonavi.png';
-import tmap from '../static/img/icon/tmap.png';
-import navermap from '../static/img/icon/navermap.png';
-import common from '../static/common/common.json';
-import '../static/css/KakaoMap.css';
+import React, { useState, useEffect } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import useCollapse from "react-collapsed";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import ToastMessage from "../components/common/ToastMessage.js";
+import coupleIcon from "../static/img/icon/coupleCharacter.png";
+import copyIcon from "../static/img/icon/copyIcon.png";
+import kakaonavi from "../static/img/icon/kakaonavi.png";
+import tmap from "../static/img/icon/tmap.png";
+import navermap from "../static/img/icon/navermap.png";
+import common from "../static/common/common.json";
+import "../static/css/KakaoMap.css";
 function KakaoMap() {
-    const [ToastStatus, setToastStatus] = useState(false);
-    const activeToast = () => {
-        setToastStatus(true);
-    };
+  const [ToastStatus, setToastStatus] = useState(false);
+  const activeToast = () => {
+    setToastStatus(true);
+  };
 
-    const runTmap = () => {
-        window.location.href = 'tmap://search?name=아펠가모잠실';
-    };
+  const {
+    getCollapseProps: getWalkCollapseProps,
+    getToggleProps: getWalkToggleProps,
+    isExpanded: isWalkExpanded,
+  } = useCollapse();
+  const {
+    getCollapseProps: getCarCollapseProps,
+    getToggleProps: getCarToggleProps,
+    isExpanded: isCarExpanded,
+  } = useCollapse();
+  const {
+    getCollapseProps: getParkingCollapseProps,
+    getToggleProps: getParkingToggleProps,
+    isExpanded: isParkingExpanded,
+  } = useCollapse();
+  const {
+    getCollapseProps: getBuffetCollapseProps,
+    getToggleProps: getBuffetToggleProps,
+    isExpanded: isBuffetExpanded,
+  } = useCollapse();
 
-    const runNaverMap = () => {
-        window.location.href = 'nmap://search?query=아펠가모잠실&appname=wedding-invite-app';
-    };
+  const runTmap = () => {
+    window.location.href = "tmap://search?name=아펠가모잠실";
+  };
 
-    const rumKakaoNavi = () => {
-        const kakao = window.Kakao;
-        if (!kakao.isInitialized()) {
-            kakao.init(common.KAKAO_JAVASCRIPT_KEY);
-        }
-        Kakao.Navi.start({
-            name: '아펠가모 잠실',
-            x: 127.0995839,
-            y: 37.51606,
-            coordType: 'wgs84',
-        });
-    };
+  const runNaverMap = () => {
+    window.location.href =
+      "nmap://search?query=아펠가모잠실&appname=wedding-invite-app";
+  };
 
-    useEffect(() => {
-        if (ToastStatus) {
-            setTimeout(() => setToastStatus(false), 1000);
-        }
-    }, [ToastStatus]);
+  const rumKakaoNavi = () => {
+    const kakao = window.Kakao;
+    if (!kakao.isInitialized()) {
+      kakao.init(common.KAKAO_JAVASCRIPT_KEY);
+    }
+    Kakao.Navi.start({
+      name: "아펠가모 잠실",
+      x: 127.0995839,
+      y: 37.51606,
+      coordType: "wgs84",
+    });
+  };
 
-    return (
-        <div id="kakaoMapDiv">
-            <p className="subTitle">오시는길</p>
-            <Map
-                id="kakaoMap"
-                center={{lat: 37.5145, lng: 127.0995839}}
-                style={{width: '100%', height: '180px'}}
-                level={5}
-                draggable={false}
-                zoomable={false}
-            >
-                <MapMarker
-                    position={{lat: 37.51606, lng: 127.0995839}}
-                    image={{
-                        src: coupleIcon,
-                        size: {
-                            width: 35,
-                            height: 35,
-                        },
-                        options: {
-                            offset: {x: 17, y: 47},
-                        },
-                    }}
-                />
-                <div className="navigationAppBtn">
-                    <button onClick={runTmap}>
-                        <img src={tmap} />
-                    </button>
-                    <button id="start-navigation" onClick={rumKakaoNavi}>
-                        <img src={kakaonavi} />
-                    </button>
-                    <button onClick={runNaverMap}>
-                        <img src={navermap} />
-                    </button>
-                </div>
+  useEffect(() => {
+    if (ToastStatus) {
+      setTimeout(() => setToastStatus(false), 1000);
+    }
+  }, [ToastStatus]);
 
-                <div style={{textAlign: 'left', marginTop: '15px'}}>
-                    <span style={{fontSize: '15px', fontWeight: 'bold'}}>
-                        아펠가모 잠실, 2층 단독웨딩홀
-                    </span>
-                    <br />
-                    <span style={{fontSize: '13px'}}>
-                        서울특별시 송파구 올림픽로35길 137 (신천동),
-                        <br />
-                        한국광고문화회관 2층&nbsp;&nbsp;
-                        <CopyToClipboard
-                            text="서울특별시 송파구 올림픽로35길 137 한국광고문화회관"
-                            onCopy={activeToast}
-                        >
-                            <span className="copyText">
-                                <img className="prefixIcon2" src={copyIcon} />
-                                &nbsp;주소복사
-                            </span>
-                        </CopyToClipboard>
-                    </span>
-                    <div>
-                        <span style={{fontSize: '14px', fontWeight: 'bold'}}>{'‣ '}도보안내</span>
-                        <br />
-                        <span style={{fontSize: '13px'}}>
-                            잠실역 7번 출구 200m 직진, Tower730 빌딩에서 우회전 (도보 5분)
-                        </span>
-                    </div>
-                    <div>
-                        <span style={{fontSize: '14px', fontWeight: 'bold'}}>{'‣ '}차량안내</span>
-                        <br />
-                        <span style={{fontSize: '13px'}}>
-                            - 잠실역에서 잠실대교 방면 200m 직진 {'>'} 잠실나루역 방향 우회전{' > '}
-                            170m 후 우회전{' > '}한국광고문화회관
-                        </span>
-                        <br />
-                        <span style={{fontSize: '13px'}}>
-                            - 잠실대교에서 잠실역 방면{' > '}잠실대교남단 삼거리{' > '}
-                            잠실나루역 방향 좌회전{' > '}170m 후 우회전{' > '}
-                            한국광고문화회관
-                        </span>
-                        <br />
-                    </div>
-                    <div>
-                        <span style={{fontSize: '14px', fontWeight: 'bold'}}>{'‣ '}주차안내</span>
-                        <br />
-                        <span style={{fontSize: '13px'}}>건물 내 지하주차장 2시간 무료</span>
-                        <span className="warningMsg">
-                            &nbsp;(B1층에서 주차등록 필수)
-                            <br />
-                            * 지하주차장 출입구가 좁습니다.
-                            <br />
-                            SUV 등 큰 차량 진입 시 유의해 주세요.
-                        </span>
-                    </div>
-                    <div>
-                        <span style={{fontSize: '14px', fontWeight: 'bold'}}>{'‣ '}피로연안내</span>
-                        <br />
-                        <span style={{fontSize: '13px'}}>
-                            B1층 뷔페 (13:30 ~ 15:30) / * 예식시간 14:00
-                        </span>
-                        <br />
-                    </div>
-                </div>
-                <br />
-                <br />
-                <p>
-                    화환은 정중히 <b style={{color: 'red', marginRight: '2px'}}>사양</b>합니다.
-                    <br />
-                    좋은 마음만 감사히 받겠습니다.
-                </p>
-            </Map>
-            {ToastStatus && <ToastMessage msg="주소가 복사되었습니다." />}
+  return (
+    <div id="kakaoMapDiv">
+      <p className="subTitle">오시는길</p>
+      <Map
+        id="kakaoMap"
+        center={{ lat: 37.5145, lng: 127.0995839 }}
+        style={{ width: "100%", height: "180px" }}
+        level={5}
+        draggable={false}
+        zoomable={false}>
+        <MapMarker
+          position={{ lat: 37.51606, lng: 127.0995839 }}
+          image={{
+            src: coupleIcon,
+            size: {
+              width: 35,
+              height: 35,
+            },
+            options: {
+              offset: { x: 17, y: 47 },
+            },
+          }}
+        />
+        <div className="navigationAppBtn">
+          <button onClick={runTmap}>
+            <img src={tmap} />
+            <p>티맵</p>
+          </button>
+          <button id="start-navigation" onClick={rumKakaoNavi}>
+            <img src={kakaonavi} />
+            <p>카카오내비</p>
+          </button>
+          <button onClick={runNaverMap}>
+            <img src={navermap} />
+            <p>네이버지도</p>
+          </button>
         </div>
-    );
+
+        <div style={{ textAlign: "left", marginTop: "15px" }}>
+          <span style={{ fontSize: "15px", fontWeight: "bold" }}>
+            아펠가모 잠실, 2층 단독웨딩홀
+          </span>
+          <br />
+          <span style={{ fontSize: "13px" }}>
+            서울특별시 송파구 올림픽로35길 137 (신천동),
+            <br />
+            한국광고문화회관 2층&nbsp;&nbsp;
+            <CopyToClipboard
+              text="서울특별시 송파구 올림픽로35길 137 한국광고문화회관"
+              onCopy={activeToast}>
+              <span className="copyText">
+                <img className="prefixIcon2" src={copyIcon} />
+                &nbsp;주소복사
+              </span>
+            </CopyToClipboard>
+          </span>
+          <div>
+            <button className="mapToggleBtn" {...getWalkToggleProps()}>
+              <span>
+                {isWalkExpanded ? (
+                  <FaMinus color="gray" />
+                ) : (
+                  <FaPlus color="gray" />
+                )}{" "}
+                도보안내
+              </span>
+            </button>
+            <section {...getWalkCollapseProps()}>
+              <span style={{ fontSize: "13px" }}>
+                잠실역 7번 출구 200m 직진, Tower730 빌딩에서 우회전 (도보 5분)
+              </span>
+            </section>
+          </div>
+          <div>
+            <button className="mapToggleBtn" {...getCarToggleProps()}>
+              <span>
+                {isCarExpanded ? (
+                  <FaMinus color="gray" />
+                ) : (
+                  <FaPlus color="gray" />
+                )}{" "}
+                차량안내
+              </span>
+            </button>
+            <section {...getCarCollapseProps()}>
+              <span style={{ fontSize: "13px" }}>
+                1. 잠실역에서 잠실대교 방면 200m 직진 {">"} 잠실나루역 방향
+                우회전
+                {" > "}
+                170m 후 우회전{" > "}한국광고문화회관
+              </span>
+              <br />
+              <span style={{ fontSize: "13px" }}>
+                2. 잠실대교에서 잠실역 방면{" > "}잠실대교남단 삼거리{" > "}
+                잠실나루역 방향 좌회전{" > "}170m 후 우회전{" > "}
+                한국광고문화회관
+              </span>
+            </section>
+          </div>
+          <div>
+            <button className="mapToggleBtn" {...getParkingToggleProps()}>
+              <span>
+                {isParkingExpanded ? (
+                  <FaMinus color="gray" />
+                ) : (
+                  <FaPlus color="gray" />
+                )}{" "}
+                주차안내
+              </span>
+            </button>
+            <section {...getParkingCollapseProps()}>
+              <span style={{ fontSize: "13px" }}>
+                건물 내 지하주차장 2시간 무료
+              </span>
+              <span className="warningMsg">
+                &nbsp;(B1층에서 주차등록 필수)
+                <br />
+                * 지하주차장 출입구가 좁습니다.
+                <br />
+                SUV 등 큰 차량 진입 시 유의해 주세요.
+              </span>
+            </section>
+          </div>
+          <div>
+            <button className="mapToggleBtn" {...getBuffetToggleProps()}>
+              <span>
+                {isBuffetExpanded ? (
+                  <FaMinus color="gray" />
+                ) : (
+                  <FaPlus color="gray" />
+                )}{" "}
+                피로연안내
+              </span>
+            </button>
+            <section {...getBuffetCollapseProps()}>
+              <span style={{ fontSize: "13px" }}>
+                B1층 뷔페 (13:30 ~ 15:30) / * 예식시간 14:00
+              </span>
+            </section>
+          </div>
+        </div>
+        <br />
+        <br />
+        <p>
+          화환은 정중히 <b style={{ color: "red", marginRight: "2px" }}>사양</b>
+          합니다.
+          <br />
+          좋은 마음만 감사히 받겠습니다.
+        </p>
+      </Map>
+      {ToastStatus && <ToastMessage msg="주소가 복사되었습니다." />}
+    </div>
+  );
 }
 
 export default KakaoMap;
